7a2379c17802a6dde82380b1c90494f01d39d299
furkanavc1 <furkanavci039@gmail.com>
2024-02-20 22:03:06 +0300

    proxy update for live

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   modules: ["@nuxt/ui", "@pinia/nuxt"],
   nitro: {
+    routeRules: {
+      "/cats": {
+        proxy: `${process.env.CATS_API_KEY}`,
+        headers: {
+          "x-api-key": `${process.env.CATS_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    },
     devProxy: {
       "/cats": {
         target: "https://api.thecatapi.com/v1/images/search",
