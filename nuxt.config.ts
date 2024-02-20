7f0b67c08210381acf0fa8d45dde4beed7fb4173
furkanavc1 <furkanavci039@gmail.com>
2024-02-20 21:05:07 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/ui", "@pinia/nuxt"],
+  nitro: {
+    devProxy: {
+      "/cats": {
+        target: "https://api.thecatapi.com/v1/images/search",
+        changeOrigin: true,
+        headers: {
+          "x-api-key": `${process.env.CATS_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
